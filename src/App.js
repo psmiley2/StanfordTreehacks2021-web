04d3840dfc1185aad018fd2d2434c21f50637651
psmiley2 <pssmiley2@gmail.com>
2021-02-13 13:10:32 -0500

    post tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Test from "./Test";
+
 function App() {
   return (
     <div>
       Treehacks 2021
+      <br />
+      Sudarshan
+      <Test user="peter" email="pds@gmail.com/" birthday={5} />
    </div>
   );
 }
