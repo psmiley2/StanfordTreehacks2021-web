93711e2b60361b3e9d00394bce63216fcd45a7b6
psmiley2 <pssmiley2@gmail.com>
2021-02-13 17:32:33 -0500

    register component working

--- src/Register.js
+++ src/Register.js
@@ -1,18 +1,42 @@
+import axios from "axios"
 import React, { useState } from 'react'
+import { backend_url } from "./config"
 
 export default function Register() {
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("")
+
+    const registerUser = async() => {
+        let body = {
+            email,
+            password
+        }
+        await axios.post(`${backend_url}/users/register`, body).then((res) => console.log(res)).catch((err) => console.error(err))
+        
+    }
+
     return (
         <div>
+            <label for="email">Email:</label>
             <input 
+                id="email"
+                type="email"
                 value={email}
                 onChange={(e) => setemail(e.target.value)}
             />
+            <br />
+            <label for="password">Password:</label>
             <input 
+                id="password"
+                type="password"
                 value={password}
                 onChange={(e) => setpassword(e.target.value)}
             />
+            <button
+                onClick={() => registerUser()}
+            >
+                Register
+            </button>
         </div>
     )
 }
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const frontend_url = "http://localhost:3000"
+// export const backend_url = "https://treehacks2021-web.herokuapp.com"
+export const backend_url = "http://localhost:8080"
